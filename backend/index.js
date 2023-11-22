8ceef366f2fbdbc32092ca9c84f76129a46d50e7
yudhiH <yudhihukkeri@gmail.com>
2023-11-22 22:01:39 +0530

    major changes

---
+++ backend/index.js
@@ -0,0 +1,77 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+const dateTimeObject = new Date();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+});
+
+app.get("/", (req, res) => {
+  res.json("Node Application is up and running.=> "+dateTimeObject.toDateString());
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+app.post("/addVehicles", (req, res) => {
+  const q = "INSERT INTO `vehiclemaster`(`vehicleNumber`, `vehicleType`, `numberOfWheels`, `pucStartDate`, `pucEndDate`, `insuranceStartDate`, `insuranceEndDate`, `rcStartDate`, `rcEndDate`, `vehicleCapacity`, `goodType`, `comments`, `createDate`,`updatedDate`, `isDeleted`, `status`) VALUES ('?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?')";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = " DELETE FROM books WHERE id = ? ";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values,bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend.");
+});
